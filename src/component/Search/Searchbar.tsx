75308654cc438bd65d18b956bd7726115073af49
kim sun hong <kongsunk@nate.com>
2023-08-02 03:42:50 +0900

    build

--- src/component/Search/Searchbar.tsx
+++ src/component/Search/Searchbar.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import '../../scss/Searchbar.scss';
 import { FaSearch } from 'react-icons/fa';
 import axios from 'axios';
